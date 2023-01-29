90d88a980799282571db7ae53a51f2eaba2ce767
MarvinK <no-replay@sanicy.de>
2023-01-29 11:51:26 +0100

    testing

--- api/products/index.php
+++ api/products/index.php
@@ -1,37 +1,12 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-    
-    include_once '../config/database.php';
-    include_once '../class/employees.php';
-    $database = new Database();
-    $db = $database->getConnection();
-    $items = new Employee($db);
-    $stmt = $items->getEmployees();
-    $itemCount = $stmt->rowCount();
-    echo json_encode($itemCount);
-    if($itemCount > 0){
-        
-        $employeeArr = array();
-        $employeeArr["body"] = array();
-        $employeeArr["itemCount"] = $itemCount;
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            extract($row);
-            $e = array(
-                "id" => $id,
-                "serverid" => $serverid,
-                "servername" => $servername,
-                "serverdescription" => $serverdescription,
-                "image" => $image
-            );
-            array_push($employeeArr["body"], $e);
-        }
-        echo json_encode($employeeArr);
-    }
-    else{
-        http_response_code(404);
-        echo json_encode(
-            array("message" => "No record found.")
-        );
-    }
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/database.php';
+$database = new Database();
+$db = $database->getConnection();
+$statement = $db->prepare("SELECT * FROM products");
+$statement->execute();
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($data);
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+        http_response_code(404);
         echo json_encode(
             array("message" => "HELLO WORLD")
         );
